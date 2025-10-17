4bd644eddb0fd8537b985c48a05093028bda72a1
Mauro Marconetto <it.horse.arg@gmail.com>
2025-10-17 18:46:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5404643633-30be9",
+  "appId": "1:499485225624:web:8e05e6c07ce3d45f8856ed",
+  "apiKey": "AIzaSyBqHHr7dIFJH2FIxnyABzl71Uz6Dii_73c",
+  "authDomain": "studio-5404643633-30be9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "499485225624"
+};
