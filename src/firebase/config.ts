d18aeaac08e0b3e6ede8fc633a85cd64c6351704
Mauro Marconetto <it.horse.arg@gmail.com>
2025-10-17 19:48:24 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,8 @@
-// This file is intentionally left blank.
-// Firebase configuration is now loaded from environment variables.
-// See .env.local and next.config.ts
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  "projectId": "studio-5404643633-30be9",
+  "appId": "1:499485225624:web:8e05e6c07ce3d45f8856ed",
+  "apiKey": "AIzaSyBqHHr7dIFJH2FIxnyABzl71Uz6Dii_73c",
+  "authDomain": "studio-5404643633-30be9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "499485225624"
 };
